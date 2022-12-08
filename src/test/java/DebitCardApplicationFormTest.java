b55569071dff7a5fa9bee03c15701d0995dcd306
sergey.perov <sergey.perov@soft-machine.ru>
2022-12-09 01:27:22 +0300

    update

--- src/test/java/DebitCardApplicationFormTest.java
+++ src/test/java/DebitCardApplicationFormTest.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.*;
@@ -6,10 +7,13 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class DebitCardApplicationFormTest {
+    @BeforeEach
+    void openURL() {
+        open("http://localhost:9999/");
+    }
     @Test
     void shouldSubmit() {
-        open("http://localhost:9999");
-        SelenideElement form = $("form[class='form form_size_m form_theme_alfa-on-white']");
+        SelenideElement form =$("form");
         form.$("[data-test-id=name] input").setValue("Вася Пупкин");
         form.$("[data-test-id=phone] input").setValue("+79934411001");
         form.$("[data-test-id=agreement]").click();
@@ -19,81 +23,73 @@ public class DebitCardApplicationFormTest {
 
     @Test
     void shouldNotSubmitEmptyForm() {
-        open("http://localhost:9999");
-        SelenideElement form = $("form[class='form form_size_m form_theme_alfa-on-white']");
+        SelenideElement form = $("form");
         form.$("[type='button']").click();
-        form.$(".input_theme_alfa-on-white.input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
     void shouldNotSubmitEmptyName() {
-        open("http://localhost:9999");
-        SelenideElement form = $("form[class='form form_size_m form_theme_alfa-on-white']");
+        SelenideElement form = $("form");
         form.$("[data-test-id=phone] input").setValue("+79934411001");
         form.$("[data-test-id=agreement]").click();
         form.$("[type='button']").click();
-        form.$(".input_theme_alfa-on-white.input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
     void shouldNotSubmitEngName() {
-        open("http://localhost:9999");
-        SelenideElement form = $("form[class='form form_size_m form_theme_alfa-on-white']");
+        SelenideElement form = $("form");
         form.$("[data-test-id=name] input").setValue("John Doe");
         form.$("[data-test-id=phone] input").setValue("+79934411001");
         form.$("[data-test-id=agreement]").click();
         form.$("[type='button']").click();
-        form.$(".input_theme_alfa-on-white.input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
     void shouldNotSubmitSymbolName() {
-        open("http://localhost:9999");
-        SelenideElement form = $("form[class='form form_size_m form_theme_alfa-on-white']");
+        SelenideElement form = $("form");
         form.$("[data-test-id=name] input").setValue("@!");
         form.$("[data-test-id=phone] input").setValue("+79934411001");
         form.$("[data-test-id=agreement]").click();
         form.$("[type='button']").click();
-        form.$(".input_theme_alfa-on-white.input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
     void shouldNotSubmitSpaceName() {
-        open("http://localhost:9999");
-        SelenideElement form = $("form[class='form form_size_m form_theme_alfa-on-white']");
+        SelenideElement form = $("form");
         form.$("[data-test-id=name] input").setValue(" ");
         form.$("[data-test-id=phone] input").setValue("+79934411001");
         form.$("[data-test-id=agreement]").click();
         form.$("[type='button']").click();
-        form.$(".input_theme_alfa-on-white.input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
     void shouldNotSubmitEmptyPhone() {
-        open("http://localhost:9999");
-        SelenideElement form = $("form[class='form form_size_m form_theme_alfa-on-white']");
+        SelenideElement form = $("form");
         form.$("[data-test-id=name] input").setValue("Вася Пупкин");
         form.$("[data-test-id=agreement]").click();
         form.$("[type='button']").click();
-        form.$(".input_theme_alfa-on-white.input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
     @Test
     void shouldNotSubmitIncorrectPhone() {
-        open("http://localhost:9999");
-        SelenideElement form = $("form[class='form form_size_m form_theme_alfa-on-white']");
+        SelenideElement form = $("form");
         form.$("[data-test-id=name] input").setValue("Вася Пупкин");
         form.$("[data-test-id=phone] input").setValue("8-993-44-11-00");
         form.$("[data-test-id=agreement]").click();
         form.$("[type='button']").click();
-        form.$(".input_theme_alfa-on-white.input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
     @Test
-    void shouldNotSubmitWithoutAgreement() {
-        open("http://localhost:9999");
-        SelenideElement form = $("form[class='form form_size_m form_theme_alfa-on-white']");
+    void shouldSendARequestMistakeWithoutACheckbox() {
+        SelenideElement form = $("form");
         form.$("[data-test-id=name] input").setValue("Вася Пупкин");
-        form.$("[data-test-id=phone] input").setValue("+79934411001");
-        form.$("[type='button']").click();
-        form.$(".input_invalid").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
+        form.$("[data-test-id=phone] input").setValue("+79781234567");
+        form.$("[type=button]").click();
+        $(".input_invalid [class=checkbox__text]").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
     }
 }
